d2a63404660b61e12970896454134fbc13695890
Matthew Bowler <mbowler03@gmail.com>
2020-05-04 12:22:23 -0400

    initial

---
+++ app/data/friends.js
@@ -0,0 +1,85 @@
+
+var friends = [
+  {
+    name:"Barack Obama",
+    photo:"https://images.unsplash.com/photo-1580130379624-3a069adbffc5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=668&q=80",
+    scores: [
+          "5",
+          "2",
+          "4",
+          "2",
+          "2",  
+          "1",
+          "5",
+          "4",
+          "5",
+          "4"
+        ]
+  },
+  {
+  name:"George W. Bush",
+  photo:"https://images.unsplash.com/photo-1580129958560-c5e47d47a677?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=695&q=80",
+  scores: [
+        "4",
+        "3",
+        "3",
+        "3",
+        "3",  
+        "2",
+        "4",
+        "2",
+        "3",
+        "2"
+      ]
+},
+{
+name:"Donald Trump",
+photo:"https://images.unsplash.com/photo-1580128660010-fd027e1e587a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=700&q=80",
+  scores: [
+      "1",
+      "5",
+      "1",
+      "5",
+      "1",  
+      "5",
+      "1",
+      "1",
+      "1",
+      "5"
+  ]
+},
+{
+  name:"Bill Clinton",
+  photo:"https://images.unsplash.com/photo-1580128637411-80206ae868e5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=690&q=80",
+  scores: [
+        "5",
+        "3",
+        "5",
+        "1",
+        "4",  
+        "1",
+        "2",
+        "1",
+        "5",
+        "5"
+      ]
+  },
+  {
+    name:"George H. Bush",
+    photo:"https://images.unsplash.com/photo-1580129958654-1f58bded0949?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=691&q=80",
+    scores: [
+          "5",
+          "5",
+          "4",
+          "4",
+          "2",  
+          "2",
+          "5",
+          "4",
+          "4",
+          "5"
+        ]
+    }
+];
+  
+module.exports = friends;
